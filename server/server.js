9cf753e5ec06be77569c16f5db42d7c164b73f8b
Subham Beura <subhambeura20@gmail.com>
2023-01-20 17:16:43 +0530

    nextjs

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
-const express= require('express')
-const mongoose=require('mongoose')
-var cors = require('cors')
-var bodyParser = require('body-parser')
-const auth=require('./auth')
-const app=express()
+const express = require('express')
+const next = require('next');
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-app.use(cors())
-app.use(bodyParser.json());
-mongoose.connect('mongodb://127.0.0.1:27017/ekirana');
-mongoose.set('strictQuery', false);
-app.use('/auth',auth)
-app.get('/',(req,res)=>{
-    res.send("Hello Wordl")
+const auth = require('./auth')
+
+
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+
+    const app = express()
+
+    app.use(cors())
+    app.use(bodyParser.json());
+    mongoose.connect('mongodb://127.0.0.1:27017/ekirana');
+    mongoose.set('strictQuery', false);
+    app.use('/auth', auth)
+    app.get('/', (req, res) => {
+        res.send("Hello Wordl")
+    })
+    const PORT = 3000
+    app.listen(PORT, () => {
+        console.log("Server Started on ", PORT)
+    });
 })
-const PORT=4000
-app.listen(PORT,()=>{
-    console.log("Server Started on ",PORT)
-});
