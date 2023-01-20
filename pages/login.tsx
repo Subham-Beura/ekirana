238ca5c912f13f13a8d95f1838313d20984d6239
Subham Beura <subhambeura20@gmail.com>
2023-01-20 20:21:09 +0530

    Styled

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
+import Navbar from '../components/Navbar';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -13,28 +14,35 @@ const Login = () => {
     try {
       const res = await axios.post('http://localhost:4000/auth/login', { username, password });
       console.log(res.data)
-    } catch (err:any) {
+    } catch (err: any) {
       setError(err.response);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e: any) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e: any) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-      {error && <p>{error}</p>}
-    </form>
+    <>
+      <Navbar />
+      <div className='flex justify-center items-center h-[93.5vh] bg-white '>
+        <form onSubmit={handleSubmit} className='w-72 h-72 p-4 flex flex-col justify-center space-y-10 border-2 border-black'>
+          <input
+            className='outline-none border-b-2 border-black'
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e: any) => setUsername(e.target.value)}
+            />
+          <input
+            className='outline-none border-b-2 border-black'
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e: any) => setPassword(e.target.value)}
+          />
+          <button type="submit" className='border-2 h-10 border-black p-1 hover:text-white hover:bg-black hover:font-bold'>Log In</button>
+          {error && <p>{error}</p>}
+        </form>
+      </div>
+    </>
   );
 };
 export default Login;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,40 +1,48 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-const Signup=()=>{
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const router = useRouter();
-  
-    const handleSubmit = async (e: { preventDefault: () => void; }) => {
-      e.preventDefault( );
-      try {
-        const res = await axios.post('http://localhost:4000/auth/signup', { username, password });
-        console.log(res.data)
-        router.push("/login")
-      } catch (err:any) {
-        setError(err);
-      }
-    };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">signup</button>
-        {/* {error && <p>{error}</p>} */}
-      </form>
-    );
+import Navbar from '../components/Navbar';
+const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:4000/auth/signup', { username, password });
+      console.log(res.data)
+      router.push("/login")
+    } catch (err: any) {
+      setError(err);
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className='flex justify-center items-center h-[93.5vh] bg-white'>
+        <form onSubmit={handleSubmit} className="w-72 h-72 p-4 flex flex-col justify-center space-y-10 border-2 border-black">
+          <input
+            type="text"
+            className='outline-none border-b-2 border-black'
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            />
+          <input
+            type="password"
+            className='outline-none border-b-2 border-black'
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className='border-2 h-10 border-black p-1 hover:text-white hover:bg-black hover:font-bold'>Sign Up</button>
+          {/* {error && <p>{error}</p>} */}
+        </form>
+      </div>
+    </>
+  );
 }
 export default Signup
