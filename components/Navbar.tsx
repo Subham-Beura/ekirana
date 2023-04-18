9e4a7619a85338b2ea169c9eee07c67379ff0d85
Subham Beura <subhambeura20@gmail.com>
2023-04-18 23:16:44 +0530

    style navbar md

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,9 +31,9 @@ const Navbar = () => {
     <div
       className={`fixed z-10 flex h-fit  min-h-[50px] w-screen flex-col text-white ${
         isOpen ? "bg-white text-black" : navBG
-      }  md:flex-row  md:justify-between  `}
+      }    `}
     >
-      <div className="relative flex  h-[50px] items-center justify-center  md:w-fit">
+      <div className="relative flex  h-[50px] items-center justify-center   ">
         {isOpen ? (
           <AiOutlineClose
             className="absolute left-0  my-3 mx-2 h-5 w-5 md:hidden"
@@ -45,32 +45,41 @@ const Navbar = () => {
             onClick={() => setIsOpen(!isOpen)}
           />
         )}
-        <Link href="/" className="flex h-[50px] items-center">
-          <h1 className=" h-fit  w-fit text-center font-fancyFont text-4xl font-extrabold md:w-fit">
+        {
+          <div className="absolute left-0  my-3 mx-8 hidden h-fit w-fit  md:block">
+            <ul className="flex justify-between space-x-10">
+              <li>
+                <Link href="#">Mens</Link>
+              </li>
+              <li>
+                <Link href="#">Womens</Link>
+              </li>
+              <li>
+                <Link href="#">Children</Link>
+              </li>
+              <li>
+                <Link href="#">Explore</Link>
+              </li>
+            </ul>
+          </div>
+        }
+        <Link href="/" className="flex h-[50px]  items-center">
+          <h1 className=" h-fit  w-[100vw] text-center  text-3xl font-bold ">
             UTSAV
           </h1>
         </Link>
 
-        <div className="absolute right-0 flex h-[50px] w-20 flex-row items-center justify-around md:hidden ">
-          <button className="button md:w-24 ">
+        <div className="absolute right-0 mr-8 flex h-[50px] w-20 flex-row items-center justify-around ">
+          <button className="button  ">
             <AiOutlineSearch size={20} />
           </button>
-          <button className="button md:w-24 ">
+          <button className="button  ">
             <AiOutlineShoppingCart size={20} />
           </button>
         </div>
       </div>
 
       <Dropdown isOpen={isOpen} />
-
-      <div className=" hidden w-fit md:flex">
-        <Link href="/login">
-          <button className="button md:w-24 ">Login</button>
-        </Link>
-        <Link href="/signup">
-          <button className="button md:w-24 ">SignUp</button>
-        </Link>
-      </div>
     </div>
   );
 };
