a1cf9fdc25797252625ee96162536c9659ff0724
Subham Beura <subhambeura20@gmail.com>
2023-06-27 17:03:08 +0530

    add

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,42 +1,46 @@
 import Link from "next/link";
+import Login from "../pages/login";
 
 export function Dropdown(props: { isOpen: boolean }) {
   return (
-    <ul
-      className={`${
-        props.isOpen ? "h-screen" : "h-0"
-      }  z-10 mx-0 w-screen items-center  justify-between overflow-hidden bg-white px-0  text-black   `}
-    >
-      <Link
-        href={"/"}
-        className="menu-button  font-mono  text-2xl text-darkbrown"
+    <>
+      <ul
+        className={`${
+          props.isOpen ? "h-screen" : "h-0"
+        }  z-10 mx-0 w-screen items-center  justify-between overflow-hidden bg-white px-0  text-black   `}
       >
-        <li className="px-5">Men</li>
-      </Link>
-      <Link
-        href={"/"}
-        className="menu-button  font-mono  text-2xl text-darkbrown"
-      >
-        <li className="px-5">Women</li>
-      </Link>
-      <Link
-        href={"/"}
-        className="menu-button  font-mono  text-2xl text-darkbrown"
-      >
-        <li className="px-5">Children</li>
-      </Link>
-      <Link
-        href={"/"}
-        className="menu-button  font-mono  text-2xl text-darkbrown"
-      >
-        <li className="px-5">Explore</li>
-      </Link>
-      <Link
-        href={"/orders"}
-        className="menu-button font-mono text-2xl text-darkbrown"
-      >
-        <li className="px-5">My Orders</li>
-      </Link>
-    </ul>
+        <Link
+          href={"/"}
+          className="menu-button  font-mono  text-2xl text-darkbrown"
+        >
+          <li className="px-5">Men</li>
+        </Link>
+        <Link
+          href={"/"}
+          className="menu-button  font-mono  text-2xl text-darkbrown"
+        >
+          <li className="px-5">Women</li>
+        </Link>
+        <Link
+          href={"/"}
+          className="menu-button  font-mono  text-2xl text-darkbrown"
+        >
+          <li className="px-5">Children</li>
+        </Link>
+        <Link
+          href={"/"}
+          className="menu-button  font-mono  text-2xl text-darkbrown"
+        >
+          <li className="px-5">Explore</li>
+        </Link>
+        <Link
+          href={"/orders"}
+          className="menu-button font-mono text-2xl text-darkbrown"
+        >
+          <li className="px-5">My Orders</li>
+        </Link>
+      </ul>
+      <div>Login</div>
+    </>
   );
 }
